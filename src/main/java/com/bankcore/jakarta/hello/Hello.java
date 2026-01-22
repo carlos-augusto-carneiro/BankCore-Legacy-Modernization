c0a9ff3759cbb171ba4f2978f35cdc144974ad86
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2026-01-22 18:25:16 -0300

    Iniciando o projeto

---
+++ src/main/java/com/bankcore/jakarta/hello/Hello.java
@@ -0,0 +1,14 @@
+package com.bankcore.jakarta.hello;
+
+public class Hello {
+
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getHello(){
+        return name;
+    }
+}
